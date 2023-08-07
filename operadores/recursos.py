dd341debff5d6bdda91487552f8cd47dab2de366
Vinícius Xavier <vinix1508@gmail.com>
2023-08-07 11:35:06 -0300

    ver.2.1.

---
+++ operadores/recursos.py
@@ -0,0 +1,11 @@
+import math
+
+raiz = math.sqrt(16) # raiz quadrada de 16
+print(f"raiz: {raiz}")
+print(f"Tipo da var raiz: {type(raiz)}")
+
+potencia = math.pow(3,4) # potêmcia: 3 elevado à 4
+print(f"Potência: {potencia}")
+
+numero_pi = math.pi
+print(f"Valor de PI: {numero_pi}")
