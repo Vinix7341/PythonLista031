928dd12e64533fa61ba3f3fc270dbfe7b5107379
Vinícius Xavier <vinix1508@gmail.com>
2023-08-07 11:07:36 -0300

    ver.2.0.

---
+++ interpolacao_strings/interpolacao_string_%.py
@@ -0,0 +1,14 @@
+nome = "Carla Joaquina"
+idade = 27
+altura = 1.759
+
+print("Tipo da var nome: %s" % type(nome))
+print("Tipo da var nome: %s" % type(idade))
+print("Tipo da var nome: %s" % type(altura))
+
+
+
+print("Nome: %s" % nome)
+print("Nome: %d" % idade)
+print("Nome: %.2f" % altura)
+print("%s possui %d anos e tem %.2fm de altura" % (nome, idade, altura))
